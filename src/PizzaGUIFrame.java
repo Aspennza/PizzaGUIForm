ce6d3fde8b0a4e59909b4fc5a1f7bc3a3bc3f9ab
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-08 11:36:58 -0400

    Created createCrustPnl method and refactored main into PizzaGUIRunner

--- src/PizzaGUIFrame.java
+++ src/PizzaGUIFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 import javax.tools.Tool;
 import java.awt.*;
 
@@ -33,6 +35,8 @@ public class PizzaGUIFrame extends javax.swing.JFrame
     JButton clearBtn;
     JButton quitBtn;
 
+    GridBagConstraints gbc = new GridBagConstraints();
+
     public PizzaGUIFrame()
     {
         super("Pizza Order Form");
@@ -46,10 +50,42 @@ public class PizzaGUIFrame extends javax.swing.JFrame
         mainPnl.setLayout(new GridBagLayout());
         add(mainPnl);
 
+        createCrustPnl();
+
         setSize(screenWidth * 3/4, screenHeight * 3/4);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Pizza Order Form");
         setVisible(true);
+    }
+
+    private void createCrustPnl()
+    {
+        crustPnl = new JPanel();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        //may need to add a fill constraint
+
+        crustPnl.setLayout(new GridLayout(3, 1));
+        crustPnl.setBorder(new TitledBorder(new EtchedBorder(), "Crust Type"));
+
+        thinRB = new JRadioButton("Thin");
+        regularRB = new JRadioButton("Regular");
+        deepDishRB = new JRadioButton("Deep Dish");
+
+        crustPnl.add(thinRB);
+        crustPnl.add(regularRB);
+        crustPnl.add(deepDishRB);
+
+        regularRB.setSelected(true);
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(thinRB);
+        group.add(regularRB);
+        group.add(deepDishRB);
+
+        mainPnl.add(crustPnl, gbc);
     }
 }
