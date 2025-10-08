ceba77cc3b8cc75576c771cc4389e148c79e59b4
Zoe Aspenns <aspennza@mail.uc.edu>
2025-10-08 11:11:53 -0400

    Created JPanels, JRadioButtons, JComboBox, and JCheckBoxes for PizzaGUIFrame

---
+++ src/PizzaGUIFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class PizzaGUIFrame extends javax.swing.JFrame
+{
+    JPanel mainPnl;
+    JPanel crustPnl;
+    JPanel sizePnl;
+    JPanel toppingPnl;
+    JPanel orderPnl;
+    JPanel controlPnl;
+
+    JRadioButton thinBtn;
+    JRadioButton regularBtn;
+    JRadioButton deepDishBtn;
+
+    JComboBox sizeCB;
+
+    JCheckBox pepperoniCB;
+    JCheckBox sausageCB;
+    JCheckBox jalapenoCB;
+    JCheckBox onionCB;
+    JCheckBox pineappleCB;
+    JCheckBox pepperCB;
+    JCheckBox blackOliveCB;
+    JCheckBox greenOliveCB;
+
+}
